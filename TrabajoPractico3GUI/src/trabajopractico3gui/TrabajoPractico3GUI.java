a2f93867eaad33719d6ed5cb4afeb0b30ca26ec8
emanuelamaya200-source <emanuelamaya200@gmail.com>
2025-08-26 20:38:54 -0300

    Base tp Practico

---
+++ TrabajoPractico3GUI/src/trabajopractico3gui/TrabajoPractico3GUI.java
@@ -0,0 +1,22 @@
+
+package trabajopractico3gui;
+
+/*
+Integrantes: 
+Delicia Aldana 
+Amaya Leandro 
+D'Agata Fabian
+Teo Nicolas
+Daiana Sosa
+Lucas Serrano
+*/
+public class TrabajoPractico3GUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
